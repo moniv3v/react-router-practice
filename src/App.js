d176331b6d5872741cdfa328e32c2ef60b331631
Monique <moniquechan1220@gmail.com>
2018-12-06 13:54:27 +0800

    [Monique] add Route, Router, Link.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+            </ul>
+            <Route path="/about" component={() => <h2>About</h2>} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
